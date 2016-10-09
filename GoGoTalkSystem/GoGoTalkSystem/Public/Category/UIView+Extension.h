0d917746046a95e8b73607a0c12b63476ae2618b
何建新 <yiyuanhanly@126.com>
2016-10-09 11:13:10 +0800

    11111

---
+++ GoGoTalkSystem/GoGoTalkSystem/Public/Category/UIView+Extension.h
@@ -0,0 +1,19 @@
+//
+//  UIView+Extension.h
+//  GoGoTalkSystem
+//
+//  Created by 何建新 on 2016/10/9.
+//  Copyright © 2016年 何建新. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (Extension)
+@property(nonatomic, assign) CGFloat x;
+@property(nonatomic, assign) CGFloat y;
+@property(nonatomic, assign) CGFloat width;
+@property(nonatomic, assign) CGFloat height;
+@property(nonatomic, assign) CGSize size;
+@property(nonatomic, assign) CGFloat centerX;
+@property(nonatomic, assign) CGFloat centerY;
+@end
