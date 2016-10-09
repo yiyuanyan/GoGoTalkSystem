3708b2514700df6aa038060f8501dda1076b2257
何建新 <yiyuanhanly@126.com>
2016-10-09 10:54:20 +0800

    初始化

---
+++ GoGoTalkSystem/GoGoTalkSystem/Home/Controller/HomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  HomeViewController.h
+//  GoGoTalkSystem
+//
+//  Created by 何建新 on 2016/10/9.
+//  Copyright © 2016年 何建新. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeViewController : UIViewController
+
+@end
